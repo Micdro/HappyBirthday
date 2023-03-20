e71587077f337dd9ad259256e2035ae5f8735f10
micdro <sehser5@hotmail.com>
2023-03-19 21:56:01 -0400

    Created the text for the Birthday Card app

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.happybirthday
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    BirthdayGreetingWithText(message = "Happy birthday!", from = "-from, Michael")
                 }
             }
         }
@@ -30,14 +36,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BirthdayGreetingWithText(message: String, from: String) {
+    Column {
+        Text(text = message, fontSize = 36.sp, textAlign = TextAlign.Center,
+            textDecoration = TextDecoration.Underline)
+        Text(text = from, fontSize = 24.sp, textAlign = TextAlign.Center)
+    }
+    
 }
 
-@Preview(showBackground = true)
+
+@Preview(showBackground = false)
 @Composable
 fun DefaultPreview() {
     HappyBirthdayTheme {
-        Greeting("Android")
+        BirthdayGreetingWithText(message = "Happy birthday!", from = "Default")
     }
 }
